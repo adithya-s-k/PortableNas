9b86a38b98802d5851ba6f347bd93216dd8b1cd1
Adithya S K <adithyaskolavi@gmail.com>
2023-04-13 23:31:48 +0530

    client file added - Adithya S K

--- client.py
+++ client.py
@@ -3,15 +3,15 @@ import socket
 IP = socket.gethostbyname(socket.gethostname())
 HOST = "localhost"
 PORT = 4456
-# ADDR = (IP, PORT)
-ADDR = (HOST, PORT)
+ADDR = (IP, PORT)
+# ADDR = (HOST, PORT)
 FORMAT = "utf-8"
 SIZE = 1024
 
 def main():
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect(ADDR)
-    # client.connect(("raspberrypi", 5000))
+    # client.connect(ADDR)
+    client.connect(("raspberrypi", 5000))
 
     while True:
         data = client.recv(SIZE).decode(FORMAT)
@@ -44,11 +44,12 @@ def main():
                 client.send(f"{cmd}@{data[1]}".encode(FORMAT))
             elif cmd == "rm":
                 client.send(f"{cmd}@{data[1]}".encode(FORMAT))
-            elif cmd == "up":
+            elif cmd == "upload":
                 path = data[1]
-                with open(f"{path}", "r") as f:
+                with open(f"client_data/{path}", "r") as f:
                     text = f.read()
-                filename = path.split("/")[-1]
+                filename = path
+                print(filename)
                 send_data = f"{cmd}@{filename}@{text}"
                 client.send(send_data.encode(FORMAT))
             else:
